aeaea6a936624e0727db7679680456619bc40409
danleetw <44081371+danleetw@users.noreply.github.com>
2020-01-04 19:22:18 +0800

    Day26 SCRAP(scrapy crawl ptt)

--- ptt.py
+++ ptt.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from bs4 import BeautifulSoup
 
 class PttSpider(scrapy.Spider):
     name = 'ptt'
-    allowed_domains = ['www.ptt.cc']
-    start_urls = ['https://www.ptt.cc/bbs/index.html/']
+    allowed_domains = ['http://www.ptt.cc']
+    start_urls = ['https://www.ptt.cc/bbs/index.html']
+    start_urls = ['https://www.ptt.cc/bbs/movie/index.html']
+    
 
     def parse(self, response):
-        print("*"*20,response)
-        input("TEST")
+        #print(response.text)
+        
+        res=BeautifulSoup(response.body)
+        print("*"*80)
+        for title in res.select('.r-ent'):
+            print(title.select(".title")[0].select("a"))
+        print("*"*80)   
         pass
